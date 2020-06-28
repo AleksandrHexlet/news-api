80c9e97e5b37e1f766bee2d75c21d69960ea2c7e
AleksandrHexlet <46378431+AleksandrHexlet@users.noreply.github.com>
2020-06-28 16:11:39 +0300

    update auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,29 +1,58 @@
-const jwt = require('jsonwebtoken');
-const { BadAuthenticationError } = require('../constructorError/error');
-const { JWT_SECRET } = require('../config');
 
-// const { NODE_ENV } = process.env;
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const user = require("../models/users");
+ const {
+   BadAuthenticationError,
+   AccessDenied,
+   BadEmail,
 
-module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    // return res.status(401).send({ message: 'Пожалуйста пройдите авторизацию' });
-    throw new BadAuthenticationError('Пожалуйста пройдите авторизацию');
-  }
-  const token = authorization.replace('Bearer ', '');
-  // const token = req.cookies.jwt;
-  // if (!req.cookies.jwt) {
-  //   throw new BadAuthenticationError('Пожалуйста пройдите авторизацию');
-  // }
+} = require("../constructorError/error");
+const { JWT_SECRET } = require("../config");
+ 
+ const { NODE_ENV } = process.env;
+ 
+ module.exports.getUser = (req, res, next) => {
+     .catch(next);
+ };
+ 
 
-  let payload;
-  try {
-    payload = jwt.verify(
-      token, JWT_SECRET,
-    );
-  } catch (nextt) {
-    throw new BadAuthenticationError('Необходима авторизация');
-  }
-  req.user = payload; // записываем пейлоуд в объект запроса
-  return next(); // пропускаем запрос дальше
-};
+ module.exports.createUser = (req, res, next) => {
+
+  const { email, password, name } = req.body;
+   bcrypt.hash(password, 10).then((hash) => {
+     user
+       .create({
+ module.exports.createUser = (req, res, _next) => {
+       .catch(() => {
+      const err = new BadEmail("Не удалось создать пользователя.");
+         return next(err);
+       });
+   });
+module.exports.createUser = (req, res, next) => {
+ 
+ module.exports.login = (req, res, next) => {
+   const { email, password } = req.body;
+
+ user
+   .findOne({ email })
+  .select("+password")
+     .then((newUser) => {
+       if (!newUser) {
+      throw new BadAuthenticationError("Неправильные почта или пароль");
+       }
+   return bcrypt.compare(password, newUser.password).then((matched) => {
+        if (!matched) {
+          throw new AccessDenied("Неправильные почта или пароль");
+        }
+        const token = jwt.sign(
+          { _id: newUser._id },
+          NODE_ENV === "production" ? JWT_SECRET : "JWT_SECRET",
+          { expiresIn: "7d" }
+        );
+        const { email, name } = newUser;
+        return res.send({ email, name, token });
+      });
+     })
+     .catch(next);
+ }
